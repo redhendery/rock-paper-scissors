4dde72ffa2dc10a09f95c138506656e8a32dcd3b
Red Hendery <red.hendery@gmail.com>
2015-10-26 16:13:49 +1300

    added compare function for game logic

--- game.js
+++ game.js
@@ -1,8 +1,6 @@
-var userChoice = prompt("Do you wish to take a Rock, some Paper or a Pair of Scissors into battle?");
-var res = userChoice.toLowerCase();
+var userInput = prompt("Do you wish to take a Rock, some Paper or a Pair of Scissors into battle?");
+var userChoice = userInput.toLowerCase();
 computerChoice = Math.random();
-
-// add .downcase to user input
 
 if (computerChoice < 0.34) {
   computerChoice = "Rock!";
@@ -15,3 +13,40 @@ elseif (computerChoice >= 0.67) {
 else {
   computerChoice = "Paper!";
 }
+
+var compare = function(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    return "It is a tie!!!"
+  }
+
+  elseif (userChoice === "rock") {
+    if(computerChoice === "scissors") {
+      return "Rock wins for you! This time..."
+    }
+    else {
+      return "Paper wins! Bow before your cybernetic overlord!"
+    }
+  }
+
+  elseif (userChoice === "paper") {
+    if(computerChoice === "rock") {
+      return "Paper wins for you! This time..."
+    }
+    else {
+      return "Scissors wins! Artifical Intelligence crushes Human Intelligence!"
+    }
+  }
+
+  elseif (userChoice === "scissors") {
+    if(computerChoice === "paper") {
+      return "Paper wins for you! This time..."
+    }
+    else {
+      return "Rock wins! You will never defeat technology pitiful human!"
+    }
+  }
+
+}
+}
+
+compare(userChoice, computerChoice)
