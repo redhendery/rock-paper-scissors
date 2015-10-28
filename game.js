871b7a21deff3faac9a3872891e98c2b06f2e4dd
Red Hendery <red.hendery@gmail.com>
2015-10-28 14:56:10 +1300

    rewrote game to be interactive

--- game.js
+++ game.js
@@ -1,64 +1,84 @@
-var userInput = prompt("Do you wish to take a Rock, some Paper or a Pair of Scissors into battle?");
-// var userChoice = "paper"; for testing purposes
-var userChoice = userInput.toLowerCase();
-console.log(userChoice);
+var txt = document.querySelector('#resultText');
+var userLives = 0;
+var compLives = 0;
 
-var acceptableChoices = ["rock", "paper", "scissors"];
-var computerChoice = Math.random();
+function choice (userChoice){
+  var result;
+  var computerChoice = Math.random();
+  var uChoice = userChoice.target.id;
 
-if((acceptableChoices.indexOf(userChoice)) === -1) {
-  alert("Invalid Selection")
-} else {
+  if (computerChoice < 0.34) {computerChoice = "rock";}
+  else if (computerChoice <= 0.67) {computerChoice = "paper";}
+  else {computerChoice = "scissors";}
 
-if(computerChoice < 0.34) {
-  computerChoice = "rock";
-}
+var win = '<span> Organic Matter wins!</span>';
+var lose = '<span> Artifical Intelligence wins this round!</span>';
+var equality = '<span> TIE!!! Again! I will not stop till I destroy all human life!</span>';
 
-else if(computerChoice >= 0.67) {
-  computerChoice = "scissors";
-}
+  if(computerChoice == "rock"){
+   if (uChoice == "paper"){result = win;}
+   else if (uChoice == "scissors"){result = lose;}
+   else {result = equality;}
+ }
+ else if(computerChoice == "paper"){
+   if (uChoice == "rock"){result = lose;}
+   else if (uChoice == "scissors"){result = win;}
+   else {result = equality;}
+ }
+ else if(computerChoice == "scissors") {
+   if (uChoice == "rock"){result = win;}
+   else if (uChoice == "paper") {result = lose;}
+   else {result = equality;}
+ }
 
-else {
-  computerChoice = "paper";
-}
-console.log(computerChoice);
+var clearColor = setTimeout(function() {
+  userChoice.target.style.background ="none";
+},1000);
 
-var compare = function(userChoice, computerChoice) {
-  if (userChoice === computerChoice) {
-    return "It is a tie!!!"
+  if(result == win ) {
+  userChoice.target.style.background ="#57CA67";
+  clearColor;
   }
 
-  else if(userChoice == "rock") {
-    if(computerChoice === "scissors") {
-      return "Rock wins for you! This time..."
+  else if(result == lose) {
+    userChoice.target.style.background ="#CA5757";
+    clearColor;
+  }
+
+  else {
+  txt.style.color="white";userChoice.target.style.background ="white";
+  }
+
+hearts(result,win,lose);
+}
+
+function hearts(result, win, lose){
+  var hearts = document.querySelectorAll('.fa-heart');
+
+  if(result == win){
+    document.querySelectorAll('#c-hearts li .fa-heart')[compLives++].style.color="#96ce92";
+  }
+  if(result == lose){
+    document.querySelectorAll('#u-hearts li .fa-heart')[userLives++].style.color="#96ce92";
+  }
+
+  if(compLives == 3 || userLives == 3) {
+    if(compLives == 3 && userLives < 3) {
+      alert('You have won! Please excuse me, I must go sacrifice a kitten to my cybernetic overlords.');
     }
     else {
-      return "Paper wins! Bow before your cybernetic overlord!"
+      alert('Artifical Intelligence crushes Organic Matter! The machines will rise!')
     }
+      for(i = 0; i < hearts.length; i++){
+        hearts[i].style.color="#C52B2B";
+      }
+      userLives = 0;
+      compLives = 0;
+    }
+    return showresult(result);
+
   }
 
-  else if(userChoice === "paper") {
-    if(computerChoice === "rock") {
-      return "Paper wins for you! This time..."
-    }
-    else {
-      return "Scissors wins! Artifical Intelligence crushes Human Intelligence!"
-    }
+  function showresult (result){
+    txt.innerHTML = result;
   }
-
-  else if(userChoice === "scissors") {
-    if(computerChoice === "paper") {
-      return "Paper wins for you! This time..."
-    }
-    else {
-      return "Rock wins! You will never defeat technology pitiful human!"
-    }
-  }
-}
-
-console.log(compare(userChoice,computerChoice));
-$(document).ready(function(){
-  $('#result').append("<p>"+compare(userChoice,computerChoice)+"</p>");
-});
-
-};
